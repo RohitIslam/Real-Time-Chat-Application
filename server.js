22a043f102c566fac21462639e2bdc9c82db24e7
Rohit Islam <air.rohit999@gmail.com>
2019-09-11 15:04:14 +0000

    basic express configuration setted up, static directory setted up

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 const express = require("express");
+const app = express();
+const path = require("path");
+
+// Define path for express config
+const staticPath = path.join(__dirname, "/public");
+
+// Setup static directory to serve
+app.use(express.static(staticPath));
+
+//Start server at PORT 5000
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
