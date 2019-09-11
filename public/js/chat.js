450674a9ceaeaaf557acb0b72e31a8b7bc7b9d68
Rohit Islam <air.rohit999@gmail.com>
2019-09-11 16:27:44 +0000

    Configured socket.io and established connection between client and server

---
+++ public/js/chat.js
@@ -0,0 +1,1 @@
+io(); // We can access io() because of this "<script src="/socket.io/socket.io.js"></script>" script in index.html
--- server.js
+++ server.js
@@ -6,12 +6,17 @@ const socketio = require("socket.io");
 // Configuring server
 const app = express();
 const server = http.createServer(app);
+const io = socketio(server);
 
 // Define path for express config
 const publicDirectoryPath = path.join(__dirname, "/public");
 
 // Setup public directory to serve
 app.use(express.static(publicDirectoryPath));
+
+io.on("connection", () => {
+  console.log("Websocket connected");
+});
 
 // Start server at PORT 5000
 const PORT = process.env.PORT || 5000;
