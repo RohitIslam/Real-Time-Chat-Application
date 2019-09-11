e679055249a23faf34be218677739b5a41152e2c
Rohit Islam <air.rohit999@gmail.com>
2019-09-11 16:13:57 +0000

    Configured express server in different way

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require("express");
-const app = express();
 const path = require("path");
+const http = require("http");
+const socketio = require("socket.io");
+
+// Configuring server
+const app = express();
+const server = http.createServer(app);
 
 // Define path for express config
-const staticPath = path.join(__dirname, "/public");
+const publicDirectoryPath = path.join(__dirname, "/public");
 
-// Setup static directory to serve
-app.use(express.static(staticPath));
+// Setup public directory to serve
+app.use(express.static(publicDirectoryPath));
 
-//Start server at PORT 5000
+// Start server at PORT 5000
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running at port ${PORT}`));
