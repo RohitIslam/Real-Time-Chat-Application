bcb1aafcb7554cff70b54017da36e286ff70da28
Rohit Islam <air.rohit999@gmail.com>
2019-09-12 12:02:13 +0000

    Rendering dynamic templates to the browser using Mustache script

--- public/js/chat.js
+++ public/js/chat.js
@@ -3,27 +3,35 @@ const socket = io(); // We can access io() because of this "<script src="/socket
 
 // Elements
 
-const messageForm = document.querySelector("form");
-const messageFormInput = document.querySelector("input");
-const messageFormButton = document.querySelector("button");
-const sendLocationButton = document.querySelector("#send-location");
+const $messageForm = document.querySelector("form");
+const $messageFormInput = document.querySelector("input");
+const $messageFormButton = document.querySelector("button");
+const $sendLocationButton = document.querySelector("#send-location");
+const $messages = document.querySelector("#messages");
+
+// Templates
+const messageTemplate = document.querySelector("#message-template").innerHTML;
 
 //listening to 'message' call from client
-socket.on("message", msg => console.log(msg));
+socket.on("message", msg => {
+  console.log(msg);
+  const html = Mustache.render(messageTemplate, { userMessage: msg });
+  $messages.insertAdjacentHTML("beforeend", html);
+});
 
-messageForm.addEventListener("submit", event => {
+$messageForm.addEventListener("submit", event => {
   event.preventDefault();
 
-  messageFormButton.setAttribute("disabled", "disabled");
+  $messageFormButton.setAttribute("disabled", "disabled");
 
   const clientMessage = event.target.elements.inputMessage.value;
 
   // emiting 'sendMessage' event from client and sending 'clientMessage' data to server
   // 3rd argument in the soket.emit function is a callback function which is for event acknowledgement
   socket.emit("sendMessage", clientMessage, error => {
-    messageFormButton.removeAttribute("disabled");
-    messageFormInput.value = "";
-    messageFormInput.focus();
+    $messageFormButton.removeAttribute("disabled");
+    $messageFormInput.value = "";
+    $messageFormInput.focus();
 
     if (error) {
       return console.log(error);
@@ -33,8 +41,8 @@ messageForm.addEventListener("submit", event => {
 });
 
 // Getting user's location from browser
-sendLocationButton.addEventListener("click", () => {
-  sendLocationButton.setAttribute("disabled", "disabled");
+$sendLocationButton.addEventListener("click", () => {
+  $sendLocationButton.setAttribute("disabled", "disabled");
 
   if (!navigator.geolocation) {
     return alert("Geolocation is not supported by your browser");
@@ -46,7 +54,7 @@ sendLocationButton.addEventListener("click", () => {
       longitude: position.coords.longitude
     };
     socket.emit("sendLocation", location, () => {
-      sendLocationButton.removeAttribute("disabled");
+      $sendLocationButton.removeAttribute("disabled");
       console.log("Location Shared!");
     });
   });
